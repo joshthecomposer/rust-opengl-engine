7a249e98a3ae7b45cb4bb88f35a79afc1f95a16b
joshthecomposer <terminatormunky@gmail.com>
2025-02-07 19:29:42 -0600

    Fix copy files script

--- copy_files.sh
+++ copy_files.sh
@@ -6,6 +6,6 @@ mkdir -p ./target/debug/resources/models
 
 cp -r ./resources/shaders/. ./target/debug/resources/shaders
 cp -r ./resources/textures/. ./target/debug/resources/textures
-cp -r ./resources/textures/. ./target/debug/resources/models
+cp -r ./resources/models/. ./target/debug/resources/models
 cp -r . ./target/debug
 cp -r ./libs/. ./target/debug
