6d8493cc90903455a1a03009c079d72a6f6f3bbc
Josh Wise <jdwisecomposer@gmail.com>
2025-03-14 01:14:30 -0500

    Add animated models to ECS system

--- resources/shaders/depth_shader.vs
+++ resources/shaders/depth_shader.vs
@@ -1,10 +1,35 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 a_pos;
+layout (location = 3) in ivec4 bone_ids;
+layout (location = 4) in vec4 bone_weights;
 
 uniform mat4 light_space_mat;
 uniform mat4 model;
+uniform bool is_animated;
+
+const int MAX_BONE_INFLUENCE = 4;
+const int MAX_BONES = 100;
+
+uniform mat4 bone_transforms[MAX_BONES];
 
 void main()
 {
-    gl_Position = light_space_mat * model * vec4(aPos, 1.0);
+	if (is_animated) {
+		vec4 totalPosition = vec4(0.0f);
+		for(int i = 0 ; i < MAX_BONE_INFLUENCE; i++)
+		{
+			if(bone_ids[i] == -1) 
+				continue;
+			if(bone_ids[i] >=MAX_BONES) 
+			{
+				totalPosition = vec4(a_pos,1.0f);
+				break;
+			}
+			vec4 localPosition = bone_transforms[bone_ids[i]] * vec4(a_pos,1.0f);
+			totalPosition += localPosition * bone_weights[i];
+		}
+		gl_Position = light_space_mat * model * totalPosition;
+	} else {
+		gl_Position = light_space_mat * model * vec4(a_pos, 1.0);
+	}
 }
--- src/entity_manager.rs
+++ src/entity_manager.rs
@@ -3,13 +3,16 @@ use glam::{vec3, Mat4, Vec3};
 use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
 
-use crate::{enums_types::{CellType, EntityType, Transform}, grid::Grid, model::Model, some_data::{GRASSES, TREES}, sparse_set::SparseSet};
+use crate::{animation::animation::{import_bone_data, import_model_data, AniModel, Animator, Bone}, enums_types::{CellType, EntityType, Transform}, grid::Grid, model::Model, some_data::{GRASSES, TREES}, sparse_set::SparseSet};
 
 pub struct EntityManager {
     pub next_entity_id: usize,
     pub transforms: SparseSet<Transform>,
     pub entity_types: SparseSet<EntityType>,
     pub models: SparseSet<Model>,
+    pub ani_models: SparseSet<AniModel>,
+    pub animators: SparseSet<Animator>,
+    pub skellingtons: SparseSet<Bone>,
     pub rng: ChaCha8Rng,
 }
 
@@ -20,11 +23,14 @@ impl EntityManager {
             transforms: SparseSet::with_capacity(max_entities),
             entity_types: SparseSet::with_capacity(max_entities),
             models: SparseSet::with_capacity(max_entities),
+            ani_models: SparseSet::with_capacity(max_entities),
+            animators: SparseSet::with_capacity(max_entities),
+            skellingtons: SparseSet::with_capacity(max_entities),
             rng: ChaCha8Rng::seed_from_u64(1)
         }
     }
 
-    pub fn create_entity(&mut self, entity_type: EntityType, position: Vec3, scale: Vec3, model_path: &str) {
+    pub fn create_static_entity(&mut self, entity_type: EntityType, position: Vec3, scale: Vec3, model_path: &str) {
         let transform = Transform {
             position,
             rotation: Mat4::IDENTITY,
@@ -52,11 +58,45 @@ impl EntityManager {
         self.next_entity_id += 1;
     }
 
+    pub fn create_animated_entity(&mut self, entity_type: EntityType, position: Vec3, scale: Vec3, model_path: &str, animation_path: &str) {
+        let transform = Transform {
+            position,
+            rotation: Mat4::IDENTITY,
+            scale,
+        };
+
+        // let mut model = AniModel::new();
+        // let mut found = false;
+        // for m in self.ani_models.iter_mut() {
+        //     if m.value().full_path == *model_path.to_string() {
+        //         model = m.value().clone();
+        //         found = true;
+        //     }
+        // }
+
+        // if !found {
+        let (skellington, animation) = import_bone_data(animation_path);
+        dbg!(&skellington);
+        let mut model = import_model_data(model_path, &animation);
+        model.setup_opengl();
+        let mut animator = Animator::new(animation);
+        animator.set_current_animation("Run");
+        // }
+        
+        self.transforms.insert(self.next_entity_id, transform);
+        self.entity_types.insert(self.next_entity_id, entity_type);
+        self.ani_models.insert(self.next_entity_id, model);
+        self.skellingtons.insert(self.next_entity_id, skellington.clone());
+        self.animators.insert(self.next_entity_id, animator);
+
+        self.next_entity_id += 1;
+    }
+
     pub fn populate_floor_tiles(&mut self, grid: &Grid, model_path: &str) {
         for cell in grid.cells.iter() {
             let pos = cell.position;
             // pos.y -= 0.98;
-            self.create_entity(EntityType::BlockGrass, pos, vec3(1.0, 1.0, 1.0), model_path);
+            self.create_static_entity(EntityType::BlockGrass, pos, vec3(1.0, 1.0, 1.0), model_path);
         }
     }
 
@@ -86,7 +126,7 @@ impl EntityManager {
                     let offset_z = z as f32 * within;
 
                     if num < entity_data.len() {
-                        self.create_entity(
+                        self.create_static_entity(
                             entity_type.clone(),
                             vec3(cell_pos.x + offset_x + smoff, 0.0, cell_pos.z + offset_z + smoff),
                             Vec3::splat(scale),
@@ -99,6 +139,11 @@ impl EntityManager {
 
     }
 
-    pub fn update(&mut self, _delta: &f64) {
+    pub fn update(&mut self, delta: &f64, elapsed_time: f32) {
+        for animator in self.animators.iter_mut() {
+            if let Some(skellington) = self.skellingtons.get_mut(animator.key()) {
+                animator.value.update(elapsed_time, skellington);
+            }         
+        }
     }
 }
