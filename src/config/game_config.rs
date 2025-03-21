cc60c6134fbdc079c8222c27cc21d0426eae694b
Josh Wise <jdwisecomposer@gmail.com>
2025-03-20 22:31:19 -0500

    Fmod working
    It turns out 2.02.22 is the only version that works with the
    fmod api, so we have to stick to that for now.

---
+++ src/config/game_config.rs
@@ -0,0 +1,26 @@
+use std::{fs::read_to_string, collections::HashMap};
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct GameConfig {
+    pub game_title: String,
+    pub cell_size: f32,
+    pub win_width: f32,
+    pub win_height: f32,
+    pub grid_height: usize,
+    pub grid_width: usize,
+    pub vsync: bool,
+    pub debug_mode: bool,
+    pub fps_counter: bool,
+    pub sounds: HashMap<String, String>,
+}
+
+impl GameConfig {
+    pub fn load_from_file(file_name: &str) -> GameConfig {
+        println!("loading configuration from {}", &file_name);
+        let config_str = read_to_string(file_name).unwrap();
+
+        serde_json::from_str(&config_str).expect("The gameconfig file was missing")
+    }
+}
